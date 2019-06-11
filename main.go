b4ae1ca966d0788450f57f0a054ccabaf9379dd8
Nasr Al <n.almansoob1@gmail.com>
2019-06-11 16:38:18 -0500

    First commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	cards := deck{newCard(), "Ace of diamonds"}
+	cards = append(cards, "Six of spades")
+	cards.print()
+}
+
+func newCard() string {
+	return "Five of spades"
+}
